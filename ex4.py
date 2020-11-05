d0fd1e67ca024dfc6cf0aa31daad76e729ecf249
ivanvorobev1 <i.vorobiew2011@yandex.ru>
2020-11-05 19:47:59 +0800

    Упражнение номер четыре

---
+++ ex4.py
@@ -0,0 +1,21 @@
+import sys
+import argparse
+
+
+def createParser ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('name', nargs='?')
+
+    return parser
+
+
+if __name__ == '__main__':
+    parser = createParser()
+    namespace = parser.parse_args()
+
+    # print (namespace)
+
+    if namespace.name:
+        print ("Привет, {}!".format (namespace.name) )
+    else:
+        print ("Привет, мир!")
