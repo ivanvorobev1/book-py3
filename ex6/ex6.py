a0eaee4fc0a69779b180fff85904036cdd8bfd4f
ivanvorobev1 <i.vorobiew2011@yandex.ru>
2020-11-08 21:26:43 +0800

    Немного доработал файл

--- ex6/ex6.py
+++ ex6/ex6.py
@@ -2,9 +2,12 @@ import sys
 import argparse
 parser = argparse.ArgumentParser('Версия 1.0')
 
-parser.add_argument('-а', '--find', help='Короче тут типа поиск файлов в каталоге')
-parser.add_argument('-n', '--name', default='name111', help='необходимо ввести имя файла для поиска')
+parser.add_argument('-f','--file', type=str, nargs='+')
+parser.add_argument('-n', '--name', help='необходимо ввести имя файла для поиска')
+parser.add_argument('-n', '--name', help='необходимо ввести имя файла для поиска')
 
 argv = parser.parse_args()
 
-print("Привет, {}!".format (argv.name))
+for find_file in argv.file:
+    in_file=open(find_file)
+    print(in_file.read())
