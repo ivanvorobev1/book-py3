93121d69f21c388f8f8ac7c34e68c25d58eff365
ivanvorobev1 <i.vorobiew2011@yandex.ru>
2020-11-08 17:16:31 +0800

    Process

--- ex6/ex6.py
+++ ex6/ex6.py
@@ -1,9 +1,10 @@
 import sys
 import argparse
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser('Версия 1.0')
 
-parser.add_argument('-n', '--names', default='name111')
+parser.add_argument('-а', '--find', help='Короче тут типа поиск файлов в каталоге')
+parser.add_argument('-n', '--name', default='name111', help='необходимо ввести имя файла для поиска')
 
 argv = parser.parse_args()
 
-print("Привет, {}!".format (argv.names))
+print("Привет, {}!".format (argv.name))
