f59df63db0ccfc954011f5eae9cf26e0798b6435
ivanvorobev1 <i.vorobiew2011@yandex.ru>
2020-11-16 22:12:35 +0800

    Update ex6.py

--- ex6/ex6.py
+++ ex6/ex6.py
@@ -1,13 +1,41 @@
+from pathlib import Path
 import sys
 import argparse
-parser = argparse.ArgumentParser('Версия 1.0')
 
-parser.add_argument('-f','--file', type=str, nargs='+')
-parser.add_argument('-n', '--name', help='необходимо ввести имя файла для поиска')
-parser.add_argument('-n', '--name', help='необходимо ввести имя файла для поиска')
+def name_find(start, args):
+    for f in start.rglob(args.name):
+        print(f)
 
-argv = parser.parse_args()
+def type_find(start, args):
+    if args.type not in ['d','f']:
+        print(f"Unknown type: {args.type}")
+        sys.exit(1)
 
-for find_file in argv.file:
-    in_file=open(find_file)
-    print(in_file.read())
+    for f in start.rglob(args.name or "*"):
+        if args.type == "d" and f.is_dir():
+            print(f)
+        elif args.type == "f" and f.is_file():
+            print(f)
+
+
+def find_files(args):
+    start_path = Path(args.start[0])
+
+    if args.name and not args.type:
+        name_find(start_path, args)
+    elif args.type:
+        type_find(start_path, args)
+    else:
+        print("You need either --name or --type")
+        sys.exit(1)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('start', type=str, nargs=1)
+    parser.add_argument('--name', type=str)
+    parser.add_argument('--type' , type=str)
+
+    return parser.parse_args()
+
+find_files(parse_args())
